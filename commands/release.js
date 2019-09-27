46995ea1280142b300df29170234b38e8d44db52
jiaquan <jiaquan@staff.sina.com.cn>
2019-09-27 16:13:19 +0800

    init

--- commands/release.js
+++ commands/release.js
@@ -1,1 +1,18 @@
-console.log(111);
+// const fs = require('fs');
+const cwd = process.cwd();
+const { exec } = require('child_process');
+const path = require('path')
+console.log(path.resolve(__dirname, './webpack.build.prod.config.js'));
+
+exec(`webpack --config ${path.resolve(__dirname, './webpack.build.prod.config.js')}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+
+// fs.readFile(`./package.json`, 'utf8', function (err, data) {
+//   console.log(data)
+// });
